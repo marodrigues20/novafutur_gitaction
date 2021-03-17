052bdbfbddd80aee36c2d89f55f37ae7619530db
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2021-03-17 18:15:09 +0000

    Run sfproperties.sh

--- sfproperties.sh
+++ sfproperties.sh
@@ -1,3 +1,3 @@
-echo "Running Command"
+echo "Exporting Security Framework Variables - DEV"
 export PSQL_JDBC_URL="jdbc:postgresql://localhost:5432/securityframework"
 export COOKIE_DOMAIN=nova-auth
